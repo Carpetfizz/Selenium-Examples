cbda3d5d67fcdd0b9d33f63e493527009a8fe87f
Carpetfizz <carpetfizz@gmail.com>
2013-08-17 14:10:32 +0530

    Improved Login and Registration Module
    Keep log.txt in the same working directory as ts_login.py and
    ts_register.py. This file temporarily stores generated IDs for login.

---
+++ ts_login_register/ts_login.py
@@ -0,0 +1,42 @@
+import unittest
+from selenium import webdriver
+import ts_register
+
+class roommatesLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        logFile=open('log.txt','r')
+        self.email=logFile.read()
+        if(self.email==""):
+            print("No email found, running ts_register.py...")
+            registrationTest=unittest.TestLoader().loadTestsFromModule(ts_register)
+            unittest.TextTestRunner(verbosity=2).run(registrationTest)
+            self.email=logFile.read()
+        
+
+    def test_login(self):
+        driver = self.driver
+        driver.get("http://next.roommates.net")
+        button_signin = driver.find_element_by_xpath("//*[@id='welcome']/a[1]")
+        button_signin.click()
+        textField_email=driver.find_element_by_xpath("//*[@id='sign-in']/div/form/input[1]")
+        textField_email.send_keys(self.email)
+        textField_password=driver.find_element_by_xpath("//*[@id='sign-in']/div/form/input[2]")
+        textField_password.send_keys("password")
+        button_submit = driver.find_element_by_xpath("//*[@id='sign-in']/div/form/button")
+        button_submit.click()
+        #add assertions
+
+    
+
+    def tearDown(self):
+        self.driver.close()
+
+
+        
+if __name__=='__main__':
+    print("ts_login.py directly invoked")
+    unittest.main()
+else:
+    print("ts_login.py running as module")
---
+++ ts_login_register/ts_register.py
@@ -0,0 +1,48 @@
+import unittest
+from random import uniform
+from math import floor
+from selenium import webdriver
+
+
+class roommatesRegistration(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_registration(self):
+        print("Running registration test...")
+        driver = self.driver 
+        driver.get("http://next.roommates.net")      
+        button_signup = driver.find_element_by_xpath("//*[@id='welcome']/a[2]")
+        button_signup.click();
+        textField_name = driver.find_element_by_xpath("//*[@id='sign-up']/form/div[1]/input")
+        textField_name.send_keys("Test") 
+        textField_email = driver.find_element_by_xpath("//*[@id='sign-up']/form/div[2]/div[1]/div/input")
+        textField_email.send_keys(self.mailGen()) 
+        textField_password = driver.find_element_by_xpath("//*[@id='sign-up']/form/div[2]/div[2]/div/input")
+        textField_password.send_keys("password") 
+        button_submit = driver.find_element_by_xpath("//*[@id='sign-up']/form/div[2]/div[4]/button")
+        button_submit.click(); 
+        note_confirmEmail = driver.find_element_by_xpath("//*[@id='check-email']/div[2]")
+        assert note_confirmEmail #assert notification box. it will only show up, if fields are legally filled
+               
+   
+
+    def mailGen(self):
+        """log.txt must be in working directory. if you move it, make sure to change the below code"""
+        self.email = "tester%s@example.com" % floor(uniform(1,10)*100)
+        logFile=open('log.txt','w') #change first param, if you move directories
+        logFile.write('')
+        logFile.write(self.email)
+        return self.email
+    
+        
+    def tearDown(self):
+        self.driver.close();
+
+if __name__=='__main__':
+    print("ts_register.py directly invoked")
+    unittest.main()
+else:
+    print("ts_register.py imported as module")
+
