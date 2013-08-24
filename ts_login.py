00d8c4bcafda77846dea26754f1e8b7ec1e277ec
Carpetfizz <carpetfizz@gmail.com>
2013-08-24 10:34:54 -0700

    Got rid of WebDriver+, uses pure Selenium WebDriver
    readme will be updated shortly

--- ts_login.py
+++ ts_login.py
@@ -1,11 +1,11 @@
 import unittest
-from webdriverplus import WebDriver
+from selenium import webdriver
 import ts_register
+from time import sleep
 
 class roommatesLogin(unittest.TestCase):
 
     def setUp(self):
-        self.driver = WebDriver('firefox',reuse_browser=True)
         logFile=open('ts_logs/log.txt','r')
         self.email=logFile.read()
         if(self.email==""):
@@ -13,26 +13,36 @@ class roommatesLogin(unittest.TestCase):
             registrationTest=unittest.TestLoader().loadTestsFromModule(ts_register)
             unittest.TextTestRunner(verbosity=2).run(registrationTest)
             self.email=logFile.read()
+            self.driver=ts_register.getOrCreateWebDriver()
+        else:
+            print("Email found in log.txt")
+            self.driver=webdriver.Firefox()
+        if __name__=='__main__':
+            self.quitBrowser=True
         
         
 
     def test_login(self):
+        print("Running ts_login.py...")
+        print("Logging in with: "+self.email)
         driver = self.driver
         driver.get("http://next.roommates.net")
-        button_signin = driver.find_element_by_xpath("//*[@id='welcome']/a[1]")
+        assert "http://next.roommates.net/#/welcome" in driver.current_url
+        sleep(2)
+        button_signin=driver.find_element_by_xpath("//*[@id='welcome']/a[1]")
         button_signin.click()
+        assert "http://next.roommates.net/#/sign-in" in driver.current_url
         textField_email=driver.find_element_by_xpath("//*[@id='sign-in']/div/form/input[1]")
         textField_email.send_keys(self.email)
-        textField_password=driver.find_element_by_xpath("//*[@id='sign-in']/div/form/input[2]")
-        textField_password.send_keys("password")
-        button_submit = driver.find_element_by_xpath("//*[@id='sign-in']/div/form/button")
-        button_submit.click()
-        #add assertions
-
-    
-
+        textField_pass=driver.find_element_by_xpath("//*[@id='sign-in']/div/form/input[2]")
+        textField_pass.send_keys("password")
+        button_submitForm=driver.find_element_by_xpath("//*[@id='sign-in']/div/form/button")
+        button_submitForm.click()
+       
+                    
     def tearDown(self):
-        self.driver.quit()
+        if(self.quitBrowser):
+            self.driver.quit()
 
 
         
@@ -41,3 +51,4 @@ if __name__=='__main__':
     unittest.main()
 else:
     print("ts_login.py running as module")
+    
--- ts_register.py
+++ ts_register.py
@@ -1,12 +1,28 @@
 import unittest
 from random import uniform
 from math import floor
-from webdriverplus import WebDriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+DRIVER = None
+
+
+'''http://stackoverflow.com/questions/10627176/selenium-webdriver-in-python-re-using-same-web-browser-across-testcases'''
+def getOrCreateWebDriver():
+    global DRIVER
+    DRIVER = DRIVER or webdriver.Firefox()
+    return DRIVER
 
 class roommatesRegistration(unittest.TestCase):
 
     def setUp(self):
-        self.driver = WebDriver('firefox',reuse_browser=True)
+        self.driver = getOrCreateWebDriver()
+        if __name__=='__main__':
+            self.quitBrowser=True
+        else:
+            self.quitBrowser=False
 
     def test_registration(self):
         print("Running registration test...")
@@ -21,24 +37,27 @@ class roommatesRegistration(unittest.TestCase):
         textField_password = driver.find_element_by_xpath("//*[@id='sign-up']/form/div[2]/div[2]/div/input")
         textField_password.send_keys("password") 
         button_submit = driver.find_element_by_xpath("//*[@id='sign-up']/form/div[2]/div[4]/button")
-        button_submit.click(); 
+        button_submit.click();
+        button_signin=WebDriverWait(self.driver,4).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='welcome']/a[1]")))
+        assert "http://next.roommates.net/#/welcome" in driver.current_url
            
 
     def mailGen(self):
         """log.txt must be in working directory. if you move it, make sure to change the below code"""
-        self.email = "tester%s@example.com" % int(floor(uniform(1,10)*100))
+        self.email = "tester%s@example.com" % int(floor(uniform(1,10)*1000))
         logFile=open('ts_logs/log.txt','w') #change first param, if you move directories
         logFile.write('')
         logFile.write(self.email)
         return self.email
-    
-        
+
     def tearDown(self):
-        self.driver.quit();
+        if(self.quitBrowser):
+            self.driver.quit()
+    
 
 if __name__=='__main__':
     print("ts_register.py directly invoked")
     unittest.main()
 else:
     print("ts_register.py imported as module")
-
+    
